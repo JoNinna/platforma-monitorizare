dd9e1e1e98418df0f9475fbc8d342f1a6af00825
JoNinna <maria.burta@student.upt.ro>
2025-08-20 19:22:35 +0300

    cerinta 1 shell - v1

--- scripts/monitoring.sh
+++ scripts/monitoring.sh
@@ -1,1 +1,35 @@
+#! /bin/bash
 # Script bash pentru monitorizarea resurselor sistemului.
+
+logfile="system-state.log"
+export timeinterval="${timeinterval:-5}"
+
+if [[ ! -f "$logfile" ]]; then
+    touch "$logfile"
+fi
+
+while true; do
+    echo "$(date)" >> "$logfile"
+    echo "CPU Info" >> "$logfile"
+    cat /proc/cpuinfo >> "$logfile"
+
+    echo "Memory Info" >> "$logfile"
+    free -h >> "$logfile"
+
+    echo "Numarul de procese in starea de Running este: " >> "$logfile"
+    ps r | wc -l >> "$logfile"
+
+    echo "Utilizarea diskului este: " >> "$logfile"
+    df -h >> "$logfile"
+
+    echo "Numele sistemului este: " >> "$logfile"
+    hostname >> "$logfile"
+
+    echo "Interfete, adrese IP si conexiuni deschise:" >> "$logfile"
+    ip a  >> "$logfile"
+    ss -tulpn >> "$logfile"
+
+    echo "Fisierul cu informatiile de sistem urmeaza sa fie suprascris"
+    sleep "$timeinterval"
+    > "$logfile"
+done
